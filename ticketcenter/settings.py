306d8ad01e3e161909fa6d1d6da1d83cf8299b03
커뮤니티 <community@macs-MacBook-Pro.local>
2021-07-12 21:53:23 +0700

    fix bugs

--- ticketcenter/settings.py
+++ ticketcenter/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'ondo_market', 'static'),
+    os.path.join(BASE_DIR, 'ticketcenter', 'static'),
 )
 STATIC_ROOT = 'static/'
 MEDIA_URL = '/media/'
--- ticketcenter/urls.py
+++ ticketcenter/urls.py
@@ -7,4 +7,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('product/', include('apps.product.urls')),
+    path('order/', include('apps.order.urls')),
+    path('event/', include('apps.banner.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
